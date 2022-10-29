93b57f010d1f615a77f0d6fc4d3a05a25e1cbe79
MaxBasharin <112333199+MaxBasharin@users.noreply.github.com>
2022-10-29 18:56:21 +0300

    добавил запуск локального сервера

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ global.app = {
 import { copy } from './gulp/tasks/copy.js';
 import { reset } from './gulp/tasks/reset.js';
 import { html } from './gulp/tasks/html.js';
+import { server } from './gulp/tasks/server.js';
 
 // наблюдптель за изменениями в файлах
 function watcher() {
@@ -25,7 +26,7 @@ function watcher() {
 const mainTasks = gulp.parallel(copy, html);
 
 // построение сценариев
-const dev = gulp.series(reset, mainTasks, copy, watcher);
+const dev = gulp.series(reset, mainTasks, copy, gulp.parallel(watcher, server));
 
 // выполнение сценария по умолчанию
 gulp.task('default', dev);
