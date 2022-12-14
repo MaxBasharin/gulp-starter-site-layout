003e049c6e34c228f403b80e9d25aae1ddcbfd9b
MaxBasharin <112333199+MaxBasharin@users.noreply.github.com>
2022-12-14 18:30:49 +0300

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,2 @@
 import * as showMessageWB from './modules/functions.js'
 showMessageWB.showMessage()
-
-import Swiper, { Navigation, Pagination } from 'swiper';
-
-var swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
